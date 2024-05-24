60ec778fda5218a846d9862c3d02882e5f364865
sina behnam <sinabehnam@sinas-MacBook-Air.local>
2024-05-24 12:01:49 +0200

    GTA/Cityscapes Decoding

--- datasets/gta5.py
+++ datasets/gta5.py
@@ -1,10 +1,51 @@
 import os
 from torch.utils.data import Dataset
-from torchvision.io import read_image
+from torchvision.io import read_image, ImageReadMode
 import torch
 import glob
 from collections import namedtuple
 import pandas as pd
+import numpy as np
+
+cityscape_color_map = {
+    'unlabeled' :             [255,(  0,  0,  0)],
+    'ego vehicle' :           [255,(  0,  0,  0)],
+    'rectification border' :  [255,(  0,  0,  0)],
+    'out of roi' :            [255,(  0,  0,  0)],
+    'static' :                [255,(  0,  0,  0)],
+    'dynamic' :               [255,(111, 74,  0)],
+    'ground' :                [255,( 81,  0, 81)],
+    'road' :                  [0,(128, 64,128)],
+    'sidewalk' :              [1,(244, 35,232)],
+    'parking' :               [255,(250,170,160)],
+    'rail track' :            [255,(230,150,140)],
+    'building' :              [  2,( 70, 70, 70)],
+    'wall' :                  [  3,(102,102,156)],
+    'fence' :                 [  4,(190,153,153)],
+    'guard rail' :            [255,(180,165,180)],
+    'bridge' :                [255,(150,100,100)],
+    'tunnel' :                [255,(150,120, 90)],
+    'pole' :                  [  5,(153,153,153)],
+    'polegroup' :             [255,(153,153,153)],
+    'traffic light' :         [  6,(250,170, 30)],
+    'traffic sign' :          [  7,(220,220,  0)],
+    'vegetation' :            [  8,(107,142, 35)],
+    'terrain' :               [  9,(152,251,152)],
+    'sky' :                   [ 10,( 70,130,180)],
+    'person' :                [ 11,(220, 20, 60)],
+    'rider' :                 [ 12,(255,  0,  0)],
+    'car' :                   [ 13,(  0,  0,142)],
+    'truck' :                 [ 14,(  0,  0, 70)],
+    'bus' :                   [ 15,(  0, 60,100)],
+    'caravan' :               [255,(  0,  0, 90)],
+    'trailer' :               [255,(  0,  0,110)],
+    'train' :                 [ 16,(  0, 80,100)],
+    'motorcycle' :            [ 17,(  0,  0,230)],
+    'bicycle' :               [ 18,(119, 11, 32)],
+    'license plate' :         [ -1,(  0,  0,142)],
+}
+
+cityscape_color_map_df = pd.DataFrame(cityscape_color_map).T
 
 class GTA5(Dataset):
     def __init__(self, images_path, labels_path, transformer, target_transofrmer):
@@ -23,9 +64,12 @@ class GTA5(Dataset):
 
         if torch.is_tensor(idx):
             idx = idx.tolist()
-
+        #
         image = read_image(self.images_dataset.iloc[idx]['image']).float()
-        label = read_image(self.images_dataset.iloc[idx]['label'][0]).long()
+        #  Read label and transform it into rgb
+        label = read_image(self.images_dataset.iloc[idx]['label'][0],mode=ImageReadMode.RGB).long()
+
+        label = self.__decode_label__(label,cityscape_color_map_df)
 
         if self.transform:
             image = self.transform(image)
@@ -53,3 +97,13 @@ class GTA5(Dataset):
 
     def __len__(self):
         return len(self.images_filenames)
+
+    def __decode_label__(self,label : torch.Tensor,df : pd.DataFrame) -> torch.Tensor:
+        result = torch.zeros(label.size()[1:], dtype=torch.long)
+
+        # # convert to one channel
+        for i in range(19):
+            mask = torch.all(label == torch.tensor(df.loc[df[0] == i].iloc[0,1]).unsqueeze(1).unsqueeze(1), dim=0)
+            result[mask] = i
+
+        return result.unsqueeze(0)
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 
 def poly_lr_scheduler(optimizer, init_lr, iter, lr_decay_iter=1,
@@ -32,3 +33,53 @@ def fast_hist(a, b, n):
 def per_class_iou(hist):
     epsilon = 1e-5
     return (np.diag(hist)) / (hist.sum(1) + hist.sum(0) - np.diag(hist) + epsilon)
+
+
+# To perform transformation in the dataset
+class IntRangeTransformer:
+    def __init__(self, min_val=0, max_val=255):
+        self.min_val = min_val
+        self.max_val = max_val
+
+    def __call__(self, sample):
+        # Perform the transformation
+        sample = torch.clamp(sample, self.min_val, self.max_val)  # Clamp values to the specified range
+        sample = ((sample - sample.min()) / (sample.max() - sample.min())) * (self.max_val - self.min_val) + self.min_val
+        return sample.long()  # Cast to torch.long
+
+# def latency(model):
+#     # Latency and FPS Calculation
+#     iterations = 1000
+#     latency = []
+#     FPS = []
+
+#     for _ in range(iterations):
+#         image = torch.randn((4, 3, 512, 1024)).to(device)
+#         start = time.time()
+#         with torch.no_grad():  # Ensure no gradients are computed for speed
+#             output = model(image)
+#         end = time.time()
+
+#         latency_i = end - start
+#         latency.append(latency_i)
+#         FPS_i = 1 / latency_i
+#         FPS.append(FPS_i)
+
+#     # Calculate mean and standard deviation for latency and FPS
+#     mean_latency = mean(latency) * 1000  # Convert to milliseconds
+#     std_latency = stdev(latency) * 1000
+#     mean_FPS = mean(FPS)
+#     std_FPS = stdev(FPS)
+
+#     print(f"Mean Latency: {mean_latency:.2f} ms, Std Latency: {std_latency:.2f} ms")
+#     print(f"Mean FPS: {mean_FPS:.2f}, Std FPS: {std_FPS:.2f}")
+
+
+# # FLOP Calculation
+# def flop(model):
+#     image = torch.zeros((4, 3, 512, 1024)).to(device) # Check if we should change something
+#     flops = FlopCountAnalysis(model, image)
+#     print(flop_count_table(flops))
+
+# def count_parameters(model):
+#     return sum(p.numel() for p in model.parameters() if p.requires_grad)
